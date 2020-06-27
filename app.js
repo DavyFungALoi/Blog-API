d939407173099698dad2bb922c6d7d3633a75a8e
Davy <davyfungaloi@gmail.com>
2020-06-27 23:55:38 +0200

    added MongoDB database connections & some base form of data

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+const dotenv = require('dotenv').config({path: 'process.env'})
 const express = require("express");
-const jwt = require("jsonwebtoken");
 const indexRouter = require('./routes/index');
 const blogRouter = require('./routes/blog');
+const mongoose = require("mongoose")
 
 const app = express();
 
@@ -14,6 +15,20 @@ app.get("/api", (req, res) => {
   });
 });
 
+
+///
+const mongoDB = process.env.DB_CONNECT
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true});
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 app.listen(5000, () =>  {
   console.log("Server started on port 5000");
 });
+
+
+//mongodb+srv://davy:blogAPI321@blogapi-remlf.azure.mongodb.net/BlogAPI?retryWrites=true&w=majority
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,38 @@
+const User = require("../models/user");
+
 exports.getUsers = function (req, res) {
-    res.send(console.log('get Users'))
-}
+  User.find({}, "first_name last_name").exec(function (err, list_users) {
+    if (err) {
+      return next(err);
+    }
+    console.log(list_users)
+    res.send({
+      users_list: list_users,
+    });
+  });
+};
 
 exports.postUsers = function (req, res) {
-    res.send(console.log('create Users'))
-}
+  const user = new User({
+    first_name: "Obi-wan",
+    last_name: "Kenobi",
+    username: "Hellothere@kappa.com",
+    password: "generalkenobi",
+    id: 1,
+  });
+  user.save(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.send(console.log(user));
+    return;
+  });
+};
 
 exports.putUsers = function (req, res) {
-    res.send(console.log('Put Users'))
-}
+  res.send(console.log("Put Users"));
+};
 
 exports.deleteUsers = function (req, res) {
-    res.send(console.log('delete Users'))
-}
+  res.send(console.log("delete Users"));
+};
