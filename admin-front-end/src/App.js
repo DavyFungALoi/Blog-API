f28d203b89eda317de4a25fa680251019644d77d
Davy <davyfungaloi@gmail.com>
2020-06-28 21:50:50 +0200

    added React skeleton for both front-end reading & front-end admin

---
+++ admin-front-end/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>Welcome to the Admin-Front-End</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ read-front-end/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div>Front-End Reader</div>
+    </div>
+  );
+}
+
+export default App;
