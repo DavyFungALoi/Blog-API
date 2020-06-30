439069704ad83b8940dbf1eaf9fdffff6a0f2ae4
Davy <davyfungaloi@gmail.com>
2020-06-30 22:17:14 +0200

    added CORS, useEffect & useState to render data

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,15 @@ const express = require("express");
 const indexRouter = require('./routes/index');
 const blogRouter = require('./routes/blog');
 const mongoose = require("mongoose")
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
+
 app.use('/', indexRouter);
 app.use('/blog', blogRouter);
+
 
 app.get("/api", (req, res) => {
   res.json({
@@ -29,6 +33,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.listen(5000, () =>  {
   console.log("Server started on port 5000");
 });
+
 
 
 //mongodb+srv://davy:blogAPI321@blogapi-remlf.azure.mongodb.net/BlogAPI?retryWrites=true&w=majority
