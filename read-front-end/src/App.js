19c8a62b5b4c098c0ab1c6f3cf90161a4f0ec879
Davy <davyfungaloi@gmail.com>
2020-07-01 22:36:27 +0200

    managed to display some data in react, next up is routing & making the blog look pretty

--- read-front-end/src/App.js
+++ read-front-end/src/App.js
@@ -1,12 +1,53 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-
+import Layout from "../src/components/layout/layout"
 function App() {
-  const [users, setUsers] = useState();
+  const [users, setUsers] = useState([]);
   const [hasError, setErrors] = useState(false);
 
-
   useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        'http://localhost:5000/blog/user',
+        {
+          mode: "cors",
+          method: "GET",
+        }
+      );
+      const data = await response.json();
+      setUsers(data.users_list);
+    }
+    fetchData()
+  }, []);
+
+  return (
+    <div className="App">
+      <Layout/>
+      <div>Front-End Reader hellothere</div>
+      <div>
+        {users.map(user => ( 
+          <div key={user._id}>{user.first_name}</div>
+        ))}
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+/*
+
+https://jsonplaceholder.typicode.com/posts
+{users.map(post => ( 
+          <li key={post.id}>{post.title}</li>
+        ))}
+
+*/
+/*
+useEffect(() => {
+
+  <div>{(typeof users.users_list !== 'undefined' ? users.users_list[0].first_name : '')}</div>
     async function fetchData() {
       const res = await fetch("http://localhost:5000/blog/user", {
         mode: "cors",
@@ -17,18 +58,14 @@ function App() {
         .then((res) => setUsers(res))
         .catch((err) => setErrors(err));
     }
+    fetchData()
+  }, []);
 
-    fetchData();
-  });
-  return (
-    <div className="App">
-      <div>Front-End Reader hellothere</div>
-      <div>{JSON.stringify(users)}</div>
-    </div>
-  );
-}
 
-export default App;
+    
+      <div>{(typeof users.users_list !== 'undefined' ? users.users_list[0].first_name : '')}</div>
+      <div>{(typeof users.users_list !== 'undefined' ? users.users_list[0].last_name : '')}</div>
+*/
 /*
     .then(function (response) {
       return response.json();
@@ -36,6 +73,8 @@ export default App;
     .then(function (response) {
       console.log(response);
     });
+
+
     
 */
 /*
