2c37aebb14b550103395ceaa4acba3f1002f0883
Davy <davyfungaloi@gmail.com>
2020-07-11 11:28:11 +0200

    added the ability to render the author by introducing an empty author object in UseEffect

--- read-front-end/src/components/Blog/Blogpost/blogpost.js
+++ read-front-end/src/components/Blog/Blogpost/blogpost.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
 
-
 export default function Blogpost({ match }) {
   useEffect(() => {
     fetchBlog();
     fetchComments();
   }, []);
-  const [blog, setBlog] = useState([]);
+  const [blog, setBlog] = useState({ author: {} });
   const [comments, setComments] = useState([]);
-
+  //{ images: {} })
+  ///[{author:{}}]
   const fetchBlog = async () => {
     const data = await fetch(
       `http://localhost:5000${window.location.pathname}`,
@@ -18,13 +18,13 @@ export default function Blogpost({ match }) {
       }
     );
     const items = await data.json();
+    console.log(items.post_data);
     setBlog(items.post_data);
   };
   const fetchComments = async () => {
-    const variable = (window.location.pathname.split( '/' ))
-    const commentVariable=variable[2]
+    const variable = window.location.pathname.split("/");
+    const commentVariable = variable[2];
     const data = await fetch(
-      
       `http://localhost:5000${window.location.pathname}/comment/${commentVariable}`,
       {
         mode: "cors",
@@ -38,16 +38,21 @@ export default function Blogpost({ match }) {
   return (
     <div>
       <div>
-        <div>blogpost</div>
-        <div>{blog.title}</div>
-        {comments.map((comment) => (
-          
-          <div key={comment._id} className="comment_overview"> 
-            <div>{comment.title}</div>
-            <div>{comment.body}</div>
-            <div>{comment.time}</div>
+        <div className="blogpost__container">
+          <div className="blogpost__container__summary">
+            <div>{blog.title}</div>
+            <div>{blog.time}</div>
+            <div>{blog.author.first_name}</div>
           </div>
-        ))}
+
+          {comments.map((comment) => (
+            <div key={comment._id} className="blogpost__comment_overview">
+              <div>{comment.title}</div>
+              <div>{comment.body}</div>
+              <div>{comment.time}</div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
