88b5f40ad9258fb9484d5caa0019cd378383ee12
Davy <davyfungaloi@gmail.com>
2020-07-29 22:07:45 +0200

    added form skeleton for submitting new posts

--- admin-front-end/src/components/CreatePosts/createpost.js
+++ admin-front-end/src/components/CreatePosts/createpost.js
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
-export default function createpost() {
+export default function Createpost() {
+    const [username, setUsername] = useState({ username: "" });
+    const [title, setTitle] = useState({ title: "" });
+    const [commentInput, setCommentInput] = useState({ commentInput: "" });
+  
+    const handleUsernameChange = (e) => {
+      setUsername({ username: e.target.value });
+    };
+  
+    const handleTitleChange = (e) => {
+      setTitle({ title: e.target.value });
+    };
+  
+    const handleCommentChange = (e) => {
+      setCommentInput({ commentInput: e.target.value });
+    };
+  
+    const handlePostCommentAndRetrieve = async () => {
+     console.log("hello")
+    };
+  
+  
     return (
+      <div>
         <div>
-            Create new Post here
+          Create new Post here
+          <form
+            className="blogpost_postCommentAndRetrieveHandler_container__commentform"
+            action=""
+          >
+            <label htmlFor="username">Username (optional)</label>
+            <input
+              name="username"
+              type="text"
+              value={username.username}
+              onChange={handleUsernameChange}
+            />
+            <label htmlFor="title">title</label>
+            <input
+              name="title"
+              type="text"
+              value={title.title}
+              onChange={handleTitleChange}
+            />
+            <label htmlFor="body">comment</label>
+            <input
+              name="body"
+              type="text"
+              value={commentInput.commentInput}
+              onChange={handleCommentChange}
+            />
+  
+            <button onClick={handlePostCommentAndRetrieve}>Submit Comment</button>
+          </form>
         </div>
-    )
+      </div>
+    );
+  
 }
+
+
+  
