eba2bdf5acb3e4c0b66331b18303191813929308
Davy <davyfungaloi@gmail.com>
2020-07-30 08:51:29 +0200

    added a form for creating a post

--- admin-front-end/src/components/CreatePosts/createpost.js
+++ admin-front-end/src/components/CreatePosts/createpost.js
@@ -1,64 +1,66 @@
 import React, { useState, useEffect } from "react";
 
 export default function Createpost() {
-    const [username, setUsername] = useState({ username: "" });
-    const [title, setTitle] = useState({ title: "" });
-    const [commentInput, setCommentInput] = useState({ commentInput: "" });
-  
-    const handleUsernameChange = (e) => {
-      setUsername({ username: e.target.value });
-    };
-  
-    const handleTitleChange = (e) => {
-      setTitle({ title: e.target.value });
-    };
-  
-    const handleCommentChange = (e) => {
-      setCommentInput({ commentInput: e.target.value });
-    };
-  
-    const handlePostCommentAndRetrieve = async () => {
-     console.log("hello")
-    };
-  
-  
-    return (
+  const [username, setUsername] = useState({ username: "" });
+  const [title, setTitle] = useState({ title: "" });
+  const [commentInput, setCommentInput] = useState({ commentInput: "" });
+  const [bodyInput, setBodyInput] = useState({ bodyInput: "" });
+
+  const handleAuthorChange = (e) => {
+    setUsername({ username: e.target.value });
+  };
+
+  const handleTitleChange = (e) => {
+    setTitle({ title: e.target.value });
+  };
+
+  const handleBodyChange = (e) => {
+    setBodyInput({ commentInput: e.target.value });
+  };
+
+  const handleCreatePost = async () => {
+    console.log("hello");
+  };
+
+  return (
+    <div>
       <div>
-        <div>
-          Create new Post here
-          <form
-            className="blogpost_postCommentAndRetrieveHandler_container__commentform"
-            action=""
-          >
-            <label htmlFor="username">Username (optional)</label>
-            <input
-              name="username"
-              type="text"
-              value={username.username}
-              onChange={handleUsernameChange}
-            />
-            <label htmlFor="title">title</label>
-            <input
-              name="title"
-              type="text"
-              value={title.title}
-              onChange={handleTitleChange}
-            />
-            <label htmlFor="body">comment</label>
-            <input
-              name="body"
-              type="text"
-              value={commentInput.commentInput}
-              onChange={handleCommentChange}
-            />
-  
-            <button onClick={handlePostCommentAndRetrieve}>Submit Comment</button>
-          </form>
-        </div>
+        Create new Post here
+        <form
+          className="blogpost_postCommentAndRetrieveHandler_container__commentform"
+          action=""
+        >
+          <label htmlFor="title">Title</label>
+          <input
+            name="author"
+            type="text"
+            value={title.title}
+            onChange={handleTitleChange}
+          />
+          <label htmlFor="author">Author (optional)</label>
+          <input
+            name="title"
+            type="text"
+            value={username.username}
+            onChange={handleAuthorChange}
+          />
+          <label htmlFor="body">Body</label>
+          <textarea
+            name="body"
+            type="text"
+            value={bodyInput.bodyInput}
+            onChange={handleBodyChange}
+          />
+          <label htmlFor="status">Status</label>
+          <select name="status">
+          <option value="" selected disabled hidden>Choose publishing status</option>
+            <option value = "unpublished">Unpublished</option>
+            <option value = "published">Published</option>
+          </select>
+
+          <button onClick={handleCreatePost}>Create Post</button>
+        </form>
       </div>
-    );
-  
+    </div>
+  );
 }
-
-
-  
