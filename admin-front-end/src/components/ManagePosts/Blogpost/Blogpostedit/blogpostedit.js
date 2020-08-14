b2e38de1212c8004bf04988576fa8a49dacbaaee
Davy <davyfungaloi@gmail.com>
2020-08-14 17:41:25 +0200

    added the ability to render indiviudal posts with edit with proper API routes

--- admin-front-end/src/components/ManagePosts/Blogpost/Blogpostedit/blogpostedit.js
+++ admin-front-end/src/components/ManagePosts/Blogpost/Blogpostedit/blogpostedit.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import moment from "moment";
 
-export default function blogpostedit() {
-    return (
-        <div>
-            blogpostedit
+export default function Blogpostedit() {
+  const [blog, setBlog] = useState({ author: {} });
+  useEffect(() => {
+    fetchBlog();
+  }, []);
+  const fetchBlog = async () => {
+    const variable = window.location.pathname.split("/");
+    const blogpostId = variable[2];
+    const data = await fetch(
+      `http://localhost:5000/blog/${blogpostId}`,
+      {
+        mode: "cors",
+        method: "GET",
+      }
+    );
+    const items = await data.json();
+    setBlog(items.post_data);
+    console.log(items)
+  };
+
+  const handleTest = () => {
+  };
+  return (
+    <div>
+      <div className="blogpost__container">
+        <div className="blogpost__container__summary">
+          <div className="blogpost__container__summary__title">
+            {blog.title}
+          </div>
+          <div className="blogpost__container__summary__author">
+            {blog.author.first_name}
+          </div>
+          <div className="blogpost__container__summary__time">
+            {moment(blog.time).format("MMMM Do, YYYY")}
+          </div>
         </div>
-    )
+        <div>{blog.body}</div>
+        <button
+          onClick={() => {
+            handleTest();
+          }}
+        >
+          Click
+        </button>
+      </div>
+    </div>
+  );
 }
